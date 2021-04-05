dc4f02bf91c95d77417c90f5dcdd82423f691ec4
kobethuwis <kobethuwis26@gmail.com>
2021-04-05 20:12:58 +0200

    updated cv, content, css, analytics, base theme, responsiveness, ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,17 @@
 $(window).on('load', function() {
-
     $('.level-bar-inner').each(function() {
-    
         var itemWidth = $(this).data('level');
-        
         $(this).animate({
             width: itemWidth
         }, 800);
-        
     });
-
 });
 
 
 jQuery(document).ready(function($) {
-
-
     /*======= Skillset *=======*/
-    
     $('.level-bar-inner').css('width', '0');
     
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
-
 });
